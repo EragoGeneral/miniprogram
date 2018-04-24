69bd52acb9978ab68c7552510167f882e2289209
linhanrui <152668252@qq.com>
2018-04-24 20:43:41 +0800

    add weather project

--- weather/pages/index/index.js
+++ weather/pages/index/index.js
@@ -2,53 +2,137 @@
 //获取应用实例
 const app = getApp()
 
+var util = require('../../utils/util.js');
+
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    weather:{
+      // wendu: 18,
+      // ganmao: '昼夜温差较大，较易发生感冒，请适当增减衣服。体质较弱的朋友请注意防护。',
+      // yesterday:{
+      //   date:'17日 星期四',
+      //   type: '阴',
+      //   fx: '南风',
+      //   fl: '微风级',
+      //   low:'低温 8℃',
+      //   high:'高温 16℃'
+      // },
+      // forecast:[
+      //   {
+      //     date:'18日星期五',
+      //     type:'阴',
+      //     high:'高温 16℃',
+      //     low:'低温 8℃',
+      //     fengxiang:'南风',
+      //     fengli:'微风级'
+      //   },
+      //   {
+      //     date: '18日星期五',
+      //     type: '阴',
+      //     high: '高温 16℃',
+      //     low: '低温 8℃',
+      //     fengxiang: '南风',
+      //     fengli: '微风级'
+      //   },
+      //   {
+      //     date: '18日星期五',
+      //     type: '阴',
+      //     high: '高温 16℃',
+      //     low: '低温 8℃',
+      //     fengxiang: '南风',
+      //     fengli: '微风级'
+      //   },
+      //   {
+      //     date: '18日星期五',
+      //     type: '阴',
+      //     high: '高温 16℃',
+      //     low: '低温 8℃',
+      //     fengxiang: '南风',
+      //     fengli: '微风级'
+      //   },
+      //   {
+      //     date: '18日星期五',
+      //     type: '阴',
+      //     high: '高温 16℃',
+      //     low: '低温 8℃',
+      //     fengxiang: '南风',
+      //     fengli: '微风级'
+      //   }
+      // ]
+    },
+    today: '2016-11-18',
+    city: '北京',
+    inputCity: ''
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+  
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      today: util.formatTime(new Date()).split(' ')[0]
+    });
+    var self = this;
+    wx.getLocation({
+      success: function(res) {
+        wx.request({
+          url: 'http://api.map.baidu.com/geocoder/v2/?ak=ASAT5N3tnHIa4APW0SNPeXN5&location='+
+          res.latitude + ',' + res.longitude +'&output=json&pois=0',
+          data:{},
+          header:{
+            'Content-Type': 'application/json'
+          },
+          success:function(res){
+            var city = res.data.result.addressComponent.city.replace('市', '');
+            self.searchWeather(city);
+          }
+        })
+      },
     })
-  }
-})
+  },
+
+  inputing:function(e){
+    this.setData({inputCity: e.detail.value});
+  },
+
+  bindSearch: function(){
+    this.searchWeather(this.data.inputCity);
+  },
+
+  searchWeather : function(cityName) {
+    var self = this;
+    wx.request({
+      url: 'http://wthrcdn.etouch.cn/weather_mini?city='+cityName,
+      data: {},
+      header:{
+        'Content-Type': 'application/json'
+      },
+      success: function(res){
+        if(res.data.status == 1002){
+          wx.showModal({
+            title: '提示',
+            content: '输入的城市名称有误，请重新输入!',
+            showCancel: false,
+            success: function(res){
+              self.setData({
+                inputCity: ''
+              });
+            }
+          })
+        }else{
+          var weather = res.data.data;
+          for(var i=0; i<weather.forecast.length; i++){
+            var d = weather.forecast[i].date;
+            weather.forecast[i].date  = ' '+d.replace('星期', '  星期');         
+            weather.forecast[i].fengli = weather.forecast[i].fengli.replace('<![CDATA[<', '').replace(']]>','');
+          }
+          self.setData({
+            city: cityName,
+            weather:weather,
+            inputCity: ''
+          });
+        }
+      }
+    })
+  } 
+});
+
+

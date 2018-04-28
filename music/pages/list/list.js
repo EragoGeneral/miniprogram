36e89f2b7b2366357d2529194be5efebf94f0982
linhanrui <152668252@qq.com>
2018-04-28 19:40:35 +0800

    add some pages to music project

--- music/pages/list/list.js
+++ music/pages/list/list.js
@@ -1,3 +1,18 @@
+var config = require('../../config.js');
+
+var formatSeconds = function(value){
+  var time = parseFloat(value);
+  var m = Math.floor(time/60);
+  var s = time - m*60;
+
+  return [m, s].map(formatNumber).join(':');
+
+  function formatNumber(n){
+    n = n.toString();
+    return n[1] ? n : '0' + n;
+  }
+}
+
 // pages/list/list.js
 Page({
 
@@ -5,14 +20,41 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    board: '',
+    songlist: [],
+    loading: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var self = this;
+    var topid = options.type;
+    this.setData({
+      loading: true
+    });
+
+    wx.request({
+      url: config.config.hotUrl,
+      data: {topid: topid},
+      success: function(e){
+        if(e.statusCode == 200){
+          var obj = e.data.showapi_res_body.pagebean;
+          console.log(obj);
+          var songlist = obj.songlist;
+          for(var i=0; i<songlist.length;i++){
+            songlist[i].seconds = formatSeconds(songlist[i].seconds);
+          }
+          self.setData({
+            board: obj.songlist[0].albumpic_big,
+            songlist: songlist,
+            loading: false
+          });
+          wx.setStorageSync('songlist', songlist);
+        }
+      }    
+    })
   },
 
   /**
--- music/pages/play/play.js
+++ music/pages/play/play.js
@@ -1,3 +1,5 @@
+var config = require('../../config.js');
+
 // pages/play/play.js
 Page({
 
@@ -5,62 +7,109 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    song: {},
+    isPlaying: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var self = this;
+    var songid = options.songid;
+
+    if (songid === undefined) {
+      var curSong = wx.getStorageSync('curSong') || {};
+      if (curSong === undefined) {
+        var song = { songname: '未选择歌曲' };
+        this.setData({
+          song: song
+        });
+      } else {
+        this.setData({
+          song: curSong
+        });
+      }
+    } else {
+      var songlist = wx.getStorageSync('songlist') || [];
+      for (var i = 0; i < songlist.length; i++) {
+        if (songlist[i].songid == songid) {
+          this.setData({
+            song: songlist[i]
+          });
+          break;
+        }
+      }
+      wx.setStorageSync('curSong', this.data.song);
+    }
+  },
+
+  playToggle: function () {
+    var self = this;
+    if (this.data.song.songname == '未选择歌曲') {
+      return;
+    }
+
+    if (this.data.isPlaying) {
+      wx.stopBackgroundAudio();
+    } else {
+      wx.playBackgroundAudio({
+        dataUrl: self.data.song.url || self.data.song.m4a,
+        success: function (res) {
+        }
+      });
+    }
+    this.setData({
+      isPlaying: !this.data.isPlaying
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
